ecf92f096ce0fe10e7ec67e48f35c18b46b1f158
Nightdread <olver94@gmail.com>
2021-02-14 18:37:04 +0000

    v1.6

--- script.sh
+++ script.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 #output directories
 FIND_PATH="$PWD"
-HOME_NAME="$USER"
+HOME_NAME="$HOME"
 CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 FILE_NAME=HW6.txt
 NEW_FILE_NAME=$FILE_NAME.$CURRENT_TIME
-echo Dir list in $FIND_PATH
-ls -la -d | awk '{print $9, $6, $7, $8}' > /home/$HOME_NAME/$NEW_FILE_NAME
+echo Dir list in "$FIND_PATH"
+ls -la -d | awk '{print $9, $6, $7, $8}' > /"$HOME"/"$NEW_FILE_NAME"
 echo "New file name: " "$NEW_FILE_NAME"
-echo $CURRENT_TIME >> /home/$HOME_NAME/$NEW_FILE_NAME
-cat /home/$HOME_NAME/$NEW_FILE_NAME
+echo "$CURRENT_TIME" >> /"$HOME"/"$NEW_FILE_NAME"
+cat /"$HOME"/"$NEW_FILE_NAME"
 #add adress to hosts
 echo 'Check hosts file'
-if ! grep -q "myapp.com*" "/etc/hosts"
+if ! grep -q "^myapp.com*" "/etc/hosts"
 then
- echo 'Dobavlenie adresa'
+ echo 'Add adress to HOSTS'
  echo '192.168.3.1 myapp.com' >> /etc/hosts
 else
  echo 'HOSTS complete'
  fi
-
