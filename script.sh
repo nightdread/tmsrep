6c11e31bf56c96154a3a5a38895d035f9a8435ae
Nightdread <olver94@gmail.com>
2021-02-14 17:03:49 +0000

    v1.3

--- script.sh
+++ script.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #output directories
 FIND_PATH="$PWD"
+HOME_NAME="$USER"
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+FILE_NAME=HW6.txt
+NEW_FILE_NAME=$FILE_NAME.$CURRENT_TIME
 echo Dir list in $FIND_PATH
-ls -la -d | awk '{print $9, $6, $7, $8}' > hw6.txt
-cat hw6.txt
+ls -la -d | awk '{print $9, $6, $7, $8}' > /home/$HOME_NAME/$NEW_FILE_NAME
+echo "New file name: " "$NEW_FILE_NAME"
+echo $CURRENT_TIME >> /home/$HOME_NAME/$NEW_FILE_NAME
+cat /home/$HOME_NAME/$NEW_FILE_NAME
 #add adress to hosts
 echo 'Check hosts file'
 if ! grep -q myapp.com.* "/etc/hosts"
@@ -12,4 +18,4 @@ then
  echo '192.168.3.1 myapp.com' >> /etc/hosts
 else
  echo 'HOSTS complete'
- fi 
+ fi
