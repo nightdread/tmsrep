9f08a4645f7d0c5133a6d70616fbd1f2c6bc2296
Nightdread <olver94@gmail.com>
2021-02-17 19:50:31 +0000

    v2.1

--- script2.sh
+++ script2.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#1. Servers list
 home_dir="$HOME"
 i=1
 for server in $(awk -F: '{print $1}' /"$home_dir"/servers_list)
 do
  echo "Server $((i++)) : $server"
 done
+#2. SSH command
+echo "Enter username: "
+read -r username
+echo "Enter server adress: "
+read -r adress
+echo "Choose command: "
+read -r command
+ssh  "$username"@"$adress" "echo | awk -F: '{print $""$command""}' $HOME/MyProject/tmsrep/variables_file"
+#3. Uppercase
