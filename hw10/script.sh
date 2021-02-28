1f89d1b388692cba3fb76ed3ed6d556dead59dfd
Artsyom Kryvitski <olver94@gmail.com>
2021-02-28 22:55:13 +0300

    fix

--- hw10/script.sh
+++ hw10/script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-CURRENT_PATH = "$PWD"
+CURRENT_PATH="$PWD"
 #---------FIND *.sh files--------
 function find_sh {
     echo "Find *.sh files in $CURRENT_PATH"
-    sudo find $CURRENT_PATH -type f -name "*.sh"
+    sudo find "$CURRENT_PATH" -type f -name "*.sh"
 }
 
 #---------FIND *.png files-------
 function find_png {
     echo "Find *.png files in $CURRENT_PATH"
-    sudo find $CURRENT_PATH -type f -name "*.png"
+    sudo find "$CURRENT_PATH" -type f -name "*.png"
 }
 find_sh
 find_png
