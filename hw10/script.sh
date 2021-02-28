d2e2d8e9f7029c5b35030e18a1de314264afff4e
Artsyom Kryvitski <olver94@gmail.com>
2021-02-28 22:46:22 +0300

    fix

--- hw10/script.sh
+++ hw10/script.sh
@@ -3,7 +3,7 @@ CURRENT_PATH = "$PWD"
 #---------FIND *.sh files--------
 function find_sh {
     echo "Find *.sh files in $CURRENT_PATH"
-    find "$CURRENT_PATH" -type f -name "*.sh"
+    sudo find $CURRENT_PATH -type f -name "*.sh"
 }
 
 find_sh
