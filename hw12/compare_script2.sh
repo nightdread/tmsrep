d51aeb8294d39f4c32299a460491b4928eb62b43
Artsyom Kryvitski <olver94@gmail.com>
2021-03-23 13:59:26 +0300

    Add script2

---
+++ hw12/compare_script2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Compare numbers A & B.
+echo "Enter number A:";
+read a;
+
+echo "Enter number B:";
+read b;
+
+echo "Enter number C;";
+read c;
+
+if (( a > b )) && (( a > c )); then
+ echo "A is greater than B and C"
+elif (( a < b )) && (( a < c )); then
+ echo "A is less than B and C"
+elif (( a == b )) && (( a == c )); then
+ echo "A,B and C are equal"
+elif (( a > b )) && (( a == c)); then
+ echo "A is great than B, but equal wits C"
+elif (( a == b )) && (( a < c )); then
+ echo "A is equal wits B, but less than C"
+elif (( a > b )) && (( a < c )); then
+ echo "A is greater than B, but less than C"
+elif (( a < b )) && (( a == c )); then
+ echo "A is less than B, but equal with C"
+elif (( a == b )) && (( a > c )); then
+ echo "A is equal with B, but greater than C"
+elif (( a < b )) && (( a > c )); then
+ echo "A is less than B, but  greater than C"
+fi
