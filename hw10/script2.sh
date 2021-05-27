e5ab9b3bda774ef147455ea56934c9a3f259e72b
Artsiom <71598118+nightdread@users.noreply.github.com>
2021-05-27 21:57:51 +0300

    Hw10 (#16)
    * Create HW10
    
    * new script
    
    * chore: savepoint
    
    * chore: savepoint
    
    del file
    
    * fix
    
    * add function
    
    * fix
    
    * new function
    
    * New script

--- hw10/script2.sh
+++ hw10/script2.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 CURRENT_PATH="$PWD"
 #---------FIND *.sh & *.png files--------
+
+echo "Enter file type:"
+read $type
+
+
 function find_files {
     echo "Find *.sh files in $CURRENT_PATH"
-    sudo find "$CURRENT_PATH" -iregex '.*\.\(png\|sh\)$'
+    sudo find -type f $type
 }
 
 find_files
